dfa2f0a784d9d58f5d578d09033d78a135033dd6
kra53n <greasha46@gmail.com>
2025-07-05 16:54:58 +0300

    add

--- router/http/router.go
+++ router/http/router.go
@@ -21,17 +21,23 @@ func Run() {
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	jwtRouterGroup := r.Group("jwt")
+	api := r.Group("api")
+
+	v1 := api.Group("v1")
 	{
-		jwtRouterGroup.GET("/get", authController.Get)
-		jwtRouterGroup.PUT("/refresh", authController.Refresh)
+		jwt := v1.Group("jwt")
+		{
+			jwt.GET("/get", authController.Get)
+			jwt.PUT("/refresh", authController.Refresh)
+		}
+
+		user := v1.Group("user")
+		{
+			user.GET("/me", userController.Me)
+			user.GET("/logout", userController.Logout)
+		}
 	}
 
-	userRouterGroup := r.Group("user")
-	{
-		userRouterGroup.GET("/me", userController.Me)
-		userRouterGroup.GET("/logout", userController.Logout)
-	}
 
 	r.Run()
 }
--- types/types.go
+++ types/types.go
@@ -1,5 +1,11 @@
 package types
 
 type AccessToken string
+
 type RefreshToken string
+
 type RefreshTokenDB string
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
